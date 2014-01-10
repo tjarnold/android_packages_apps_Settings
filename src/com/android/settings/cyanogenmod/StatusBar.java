5131d49fb3680348a49ec3842cecaa84e1c53807
tjarnold <tjarnold@seahawksystems.com>
2014-01-10 14:44:22 -0500

    Move these back to make merging easier.
    Change-Id: I363dae9da807af2d36401cbc0a0dac195454f72a

--- src/com/android/settings/cyanogenmod/StatusBar.java
+++ src/com/android/settings/cyanogenmod/StatusBar.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2012 The CyanogenMod project
  * Copyright (C) 2013 BlackoutROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.blackout;
+package com.android.settings.cyanogenmod;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
